b2ce3df096b4c6839cd7c72ca37fec03c6ffb2fb
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2022-10-03 14:25:30 +0200

    Add project files.

---
+++ Events/Program.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+
+namespace Events
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var transaction = new Transaction();
+            transaction.TransactionComplete += AfterComplete;
+            transaction.StartTransaction();
+        }
+
+        private static void AfterComplete()
+        {
+            Console.WriteLine("Completed");
+        }
+    }
+
+    public delegate void Notify();
+
+    public class Transaction
+    {
+        public Notify TransactionComplete { get; set; }
+
+        public void StartTransaction()
+        {
+            //Do something;
+            OnTransactionComplete();
+
+        }
+
+        protected virtual void OnTransactionComplete()
+        {
+            TransactionComplete?.Invoke();
+        }
+    }
+}
